7a548d4181076395c2d7c20597d489a890d54378
Alex <aamartir@gmail.com>
2014-02-15 03:21:08 -0500

    V1.0
    Tested with BMP085_ISR and works fine. Added function to read and write
    bytes, words, and array of bytes.

--- I2C.cpp
+++ I2C.cpp
@@ -1,6 +1,6 @@
 #include "I2C.h"
 
-static void I2C::write8( uint8_t devID, uint8 reg_addr, int8_t val ) 
+void I2C::write8( uint8_t devID, uint8_t reg_addr, uint8_t val ) 
 {
   Wire.beginTransmission( devID );	// start transmission to device 
   Wire.write( reg_addr );				    // send register address
@@ -8,20 +8,20 @@ static void I2C::write8( uint8_t devID, uint8 reg_addr, int8_t val )
   Wire.endTransmission();			      // end transmission
 }
 
-static void I2C::write16( uint8_t devID, uint8_t reg_addr, int16_t val )
+void I2C::write16( uint8_t devID, uint8_t reg_addr, uint16_t val )
 {
-  write8( devID, reg_addr, val >> 8 );
-  write8( devID, reg_addr + 1, val & 0xFF )
+  I2C::write8( devID, reg_addr, val >> 8 );
+  I2C::write8( devID, reg_addr + 1, val & 0xFF );
 }
 
-static boolean I2C::readBytes( uint8_t devID, uint8_t reg_addr, uint8_t bytesToRead, int8_t *buf )
+bool I2C::readBytes( uint8_t devID, uint8_t reg_addr, uint8_t bytesToRead, uint8_t *buf )
 {
   Wire.beginTransmission(devID);	// start transmission to device 
   Wire.write(reg_addr);				    // send register address
   Wire.endTransmission();
 
   Wire.beginTransmission(devID);
-  Wire.requestFrom( devID, bytesToRead );
+  Wire.requestFrom( (int)devID, (int)bytesToRead );
 
   uint8_t i = 0;
   while( Wire.available() )
@@ -35,16 +35,16 @@ static boolean I2C::readBytes( uint8_t devID, uint8_t reg_addr, uint8_t bytesToR
 }
 
 /* Results of the read are stored in ptr */
-static int8_t I2C::read8( uint8_t devID, uint8_t reg_addr )
+uint8_t I2C::read8( uint8_t devID, uint8_t reg_addr )
 {
-  int8_t b;
+  uint8_t b;
   
-  Wire.beginTransmission(devID);	// start transmission to device 
-  Wire.write(reg_addr);				    // send register address
+  Wire.beginTransmission( devID );	// start transmission to device 
+  Wire.write( reg_addr );		// send register address
   Wire.endTransmission();
 
-  Wire.beginTransmission(devID);
-  Wire.requestFrom( devID, 1 );   // Request 1 byte from device
+  Wire.beginTransmission( devID );
+  Wire.requestFrom( (int)devID, 1 );   // Request 1 byte from device
   
   b = Wire.read();
   Wire.endTransmission();
@@ -52,13 +52,13 @@ static int8_t I2C::read8( uint8_t devID, uint8_t reg_addr )
   return ( b );
 }
 
-static int16_t I2C::read16( uint8_t devID, uint8_t reg_addr )
+uint16_t I2C::read16( uint8_t devID, uint8_t reg_addr )
 {
-  int8_t b[2];
+  uint8_t b[2];
   
   /* Read 1 word (2 bytes)*/
-  if( readBytes(devID, reg_addr, 2, b) )
-    return ( ((uint16_t) b[0] << 8) | b[1] );
+  if( I2C::readBytes(devID, reg_addr, 2, b) )
+    return ( (b[0] << 8) | b[1] );
   
   return false;
 }
@@ -66,21 +66,21 @@ static int16_t I2C::read16( uint8_t devID, uint8_t reg_addr )
 /* bitNumber is from 0 to N-1, right to left. That is, bit 0 is the 
  * least significant bit. bit 1 the 2nd least significant, and so on. 
  */
-static boolean I2C::readBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber )
+bool I2C::readBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber )
 {
-  int8_t b;
+  uint8_t b;
   
   /* Read the contents of reg_addr */
-  b = read8( devID, reg_addr ); 
+  b = I2C::read8( devID, reg_addr ); 
   return ( (b >> bitNumber) & 1 );
 }
 
-static void I2C::writeBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber, bool val )
+void I2C::writeBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber, bool val )
 {
-  int8_t b;
+  uint8_t b;
 
   /* Read the contents of reg_addr */
-  b = read8( devID, reg_addr ); 
+  b = I2C::read8( devID, reg_addr ); 
 
   /* Modify the bit in the register */
   if( val )
@@ -89,18 +89,18 @@ static void I2C::writeBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber, b
     b &= ~(val << bitNumber);
   
   /* Write new value to reg_addr */
-  write8( devID, reg_addr, b );	
+  I2C::write8( devID, reg_addr, b );	
 }
 
-static void flipBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber )
+void flipBit( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber )
 {
-  int8_t b;
+  uint8_t b;
   
   /* Read the contents of reg_addr */
-  b = read8( devID, reg_addr ); 
+  b = I2C::read8( devID, reg_addr ); 
   
   /* XOR operator */
   b ^= ( 1 << bitNumber );
   
-  write8( devID, reg_addr, b );
+  I2C::write8( devID, reg_addr, b );
 }
--- I2C.h
+++ I2C.h
@@ -7,16 +7,16 @@ class I2C
 {
   public:
     /* Write functions */
-    static void writeBit    ( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber, boolean val );
+    static void writeBit    ( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber, bool val );
     static void flipBit     ( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber );
-    static void write8      ( uint8_t devID, uint8_t reg_addr, int8_t val );
-    static void write16     ( uint8_t devID, uint8_t reg_addr, int16_t val );
+    static void write8      ( uint8_t devID, uint8_t reg_addr, uint8_t val );
+    static void write16     ( uint8_t devID, uint8_t reg_addr, uint16_t val );
     
     /* Read functions */
-    static boolean readBit  ( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber );
-    static int8_t  read8    ( uint8_t devID, uint8_t reg_addr );
-    static int16_t read16   ( uint8_t devID, uint8_t reg_addr );
-    static boolean readBytes( uint8_t devID, uint8_t reg_addr, uint8_t bytesToRead, int8_t *buf );
+    static bool readBit     ( uint8_t devID, uint8_t reg_addr, uint8_t bitNumber );
+    static uint8_t  read8    ( uint8_t devID, uint8_t reg_addr );
+    static uint16_t read16   ( uint8_t devID, uint8_t reg_addr );
+    static bool readBytes   ( uint8_t devID, uint8_t reg_addr, uint8_t bytesToRead, uint8_t *buf );
 };
 
 #endif
